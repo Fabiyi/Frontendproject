87fc34be63936de296f4219e5b19ed2fc4b01297
Fabiyi <fabiyidanieldamilola@gmail.com>
2024-12-03 13:38:00 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const { transform } = require('next/dist/build/swc');
-const { Blinker } = require('next/font/google');
+// const { transform } = require('next/dist/build/swc');
+// const { Blinker } = require('next/font/google');
 const { Fade, Slide } = require('react-slideshow-image');
 
 /** @type {import('tailwindcss').Config} */
@@ -21,39 +21,10 @@ module.exports = {
         section3: "url('/public/Images/Doctor4.jpg')",
       },
       keyframes:{
-        wiggle:{
-          '0% , 100%':
-          { transform: 'rotate(-3deg)'},
-          '50%':{ transform: 'rotate(3deg)'},
-        },
-        fadeIn:{
-          '0%': { opacity: '0'},
-          '100%': { opacity: '1'},
-        },
-        typing:{
-          '0%': { width: '0%'},
-          '100%': {width: '100%'}
-        },
-        blink: {
-          '50%': {borderColor: 'transparent'},
-        },
-          fadeIn: {
-            '0%': {opacity: '0'},
-            '100%': {opacity: '1'}
-          },
-          slideIn: {
-            '0%': {transform: 'translateY(-100%)'},
-            '100%': {transform: 'translateX(0)'},
-          },
+        
         },
         },
       },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-        fadeIn: 'fadeIn 1s ease-in forwards',
-        typing: 'typing 2s steps(20) forwards, blink 0.75s step-end infinites',
-        fadeIn: 'fadeIn 1s ease-out forwards',
-        slideIn: 'slideIn 1s ease-out forwards'
-      },
+      
   plugins: [],
 };
